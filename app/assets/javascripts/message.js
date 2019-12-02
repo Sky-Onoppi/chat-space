bb7d3de72b1996976f2150fdfe9e7d2141ae3dcd
Sky-Onoppi <onoppisih_sky@yahoo.co.jp>
2019-12-02 19:52:38 +0900

    Hidouki_2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,51 +1,26 @@
 $(function(){
 
-  function buildHTML(message){
-    // 「もしメッセージに画像が含まれていたら」という条件式
-    if (message.image) {
-      // var html = //メッセージに画像が含まれる場合のHTMLを作る
-      var content = message.content ? `${ message.content }` : "";
-      var img = message.image ? `<img src= ${ message.image }>` : "";
-      var html = `<div class="message" data-id="${message.id}">
-                    <div class="message__detail">
-                      <p class="message__detail__current-user-name">
-                        ${message.user_name}
-                      </p>
-                      <p class="message__detail__date">
-                        ${message.date}
-                      </p>
-                    </div>
-                    <p class="message_body">
-                      <div>
-                      ${content}
-                      </div>
-                      ${img}
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="message__detail">
+                    <p class="message__detail__current-user-name">
+                      ${message.user_name}
                     </p>
-                  </div>`
-    } else {
-      // var html = //メッセージに画像が含まれない場合のHTMLを作る      
-      var content = message.content ? `${ message.content }` : "";
-      // var img = message.image ? `<img src= ${ message.image }>` : "";
-      var html = `<div class="message" data-id="${message.id}">
-                    <div class="message__detail">
-                      <p class="message__detail__current-user-name">
-                        ${message.user_name}
-                      </p>
-                      <p class="message__detail__date">
-                        ${message.date}
-                      </p>
-                    </div>
-                    <p class="message_body">
-                      <div>
-                      ${content}
-                      </div>
-
+                    <p class="message__detail__date">
+                      ${message.date}
                     </p>
-                  </div>`
-    }
-    return html
+                  </div>
+                  <p class="message_body">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
   }
-  // console.log('イベント発火')
 
   $('#new_message').on('submit', function(e){
     e.preventDefault()
@@ -63,9 +38,8 @@ $(function(){
       var html = buildHTML(data);
       $('.contentsright').append(html);
       $('.contentsright').animate({ scrollTop: $('.contentsright')[0].scrollHeight});
-      // $('.messages').append(html);
-      // $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $('#message_content').val(''); //input内のメッセージを消しています。
+      $('form')[0].reset();
+      $('#message_content').val(''); 
     })
     .fail(function(data){
       alert('エラーが発生したためメッセージは送信できませんでした。');
